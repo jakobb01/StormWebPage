d5812244795a65dd1852dd7790ebd6ffb569f980
Jakob Beber <jakob.beber@cosylab.com>
2024-02-04 20:51:36 +0100

    feat: ui->added nav bar & login page

--- index.php
+++ index.php
@@ -7,43 +7,118 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 <body>
+
+
 <h1>Weather App</h1>
 
-<form id="weatherForm">
-    <label for="location">Enter Location:</label>
-    <input type="text" id="location" name="location" required>
-    <button type="button" onclick="getWeather()">Get Weather</button>
-</form>
+<nav>
+    <button onclick="showSearchUI()">SEARCH</button>
+    <button onclick="showHistoryUI()">HISTORY</button>
+    <button onclick="showLoginUI()">LOGIN</button>
+</nav>
 
-<div id="weatherInfo">
-    <!-- Weather information will be displayed here -->
+<div id="searchUI" style="display: none;">
+    <br>
+    <br>
+    <form id="weatherForm">
+        <label for="location">Enter Location:</label>
+        <input type="text" id="location" name="location" required>
+        <button type="button" onclick="getWeather()">Get Weather</button>
+    </form>
+
+    <div id="weatherInfo">
+        <!-- Weather information will be displayed here -->
+    </div>
+
+    <script>
+        function getWeather() {
+            var location = document.getElementById('location').value;
+
+            // Send AJAX request to weather.php with the location
+            $.ajax({
+                type: 'GET',
+                url: 'weather.php',
+                data: {location: location},
+                success: function (xmlData) {
+                    // Extract information from XML
+                    var locationInfo = xmlData.getElementsByTagName("location")[0].childNodes[0].nodeValue;
+                    var temperatureInfo = xmlData.getElementsByTagName("temperature")[0].childNodes[0].nodeValue;
+
+                    // Display information on the webpage
+                    var weatherInfoHtml = '<p>Location: ' + locationInfo + '</p>';
+                    weatherInfoHtml += '<p>Temperature: ' + temperatureInfo + ' °C</p>';
+
+                    $('#weatherInfo').html(weatherInfoHtml);
+                },
+                error: function () {
+                    // Handle errors here
+                    $('#weatherInfo').html('<p>Error fetching weather information.</p>');
+                }
+            });
+        }
+    </script>
+</div>
+
+<div id="historyUI" style="display: none;">
+    <h2>History UI</h2>
+    <!-- Add content for the History UI here -->
+</div>
+
+<div id="loginUI" style="display: none;">
+    <h2>Login or Register</h2>
+
+    <form id="loginForm">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br>
+        <br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <br>
+
+        <button type="button" onclick="login()">Login</button>
+        <button type="button" onclick="register()">Register</button>
+    </form>
+
+    <script>
+        function login() {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+
+            // Perform login logic here
+            console.log('Login:', name, email);
+        }
+
+        function register() {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+
+            // Perform registration logic here
+            console.log('Register:', name, email);
+        }
+    </script>
 </div>
 
 <script>
-    function getWeather() {
-        var location = document.getElementById('location').value;
+    function showSearchUI() {
+        hideAllUIs();
+        $('#searchUI').show();
+    }
 
-        // Send AJAX request to weather.php with the location
-        $.ajax({
-            type: 'GET',
-            url: 'weather.php',
-            data: { location: location },
-            success: function(xmlData) {
-                // Extract information from XML
-                var locationInfo = xmlData.getElementsByTagName("location")[0].childNodes[0].nodeValue;
-                var temperatureInfo = xmlData.getElementsByTagName("temperature")[0].childNodes[0].nodeValue;
+    function showHistoryUI() {
+        hideAllUIs();
+        $('#historyUI').show();
+    }
 
-                // Display information on the webpage
-                var weatherInfoHtml = '<p>Location: ' + locationInfo + '</p>';
-                weatherInfoHtml += '<p>Temperature: ' + temperatureInfo + ' °C</p>';
+    function showLoginUI() {
+        hideAllUIs();
+        $('#loginUI').show();
+    }
 
-                $('#weatherInfo').html(weatherInfoHtml);
-            },
-            error: function() {
-                // Handle errors here
-                $('#weatherInfo').html('<p>Error fetching weather information.</p>');
-            }
-        });
+    function hideAllUIs() {
+        $('#searchUI, #historyUI, #loginUI').hide();
     }
 </script>
 
