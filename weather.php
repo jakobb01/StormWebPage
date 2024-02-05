fa7d6061ce76d533f6718dbd1143de1a4020cd62
Jakob Beber <jakob.beber@cosylab.com>
2024-02-05 14:00:41 +0100

    test: testing new features for weather

--- weather.php
+++ weather.php
@@ -52,51 +52,73 @@ function getCoordinates( $city ) {
     return $response;
 }
 
+$wmo = [0, 1, 2, 3, 45, 48, 51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 71, 73, 75, 77, 80, 81, 82, 85, 86, 95, 96, 99];
+$wmo_name = ["clear sky", "mainly clear", "partly cloudy", "overcast", "fog", "depositing rime fog", "light drizzle", "moderate drizzle", "dense drizzle", "slight rain", "moderate rain", "heavy rain", "freezing rain: light", "freezing rain: heavy", "slight snow fall", "moderate snow fall", "heavy snow fall", "snow grains", "slight rain showers", "moderate rain shower", "violent rain shower", "slight snow shower", "heavy snow shower", "thunderstorm", "thunderstorm", "thunderstorm"];
+
 
 // Check if the location is provided in the GET request
 if (isset($_GET['location'])) {
 
-    $location = urlencode($_GET['location']);
-    $coordinates_json = getCoordinates($location);
-    $coordinates = json_decode($coordinates_json, true);
-    $lat = $coordinates[0]['latitude'];
-    $lon = $coordinates[0]['longitude'];
+    //$location = urlencode($_GET['location']);
+    //$coordinates_json = getCoordinates($location);
+    //$coordinates = json_decode($coordinates_json, true);
+    //$lat = $coordinates[0]['latitude'];
+    //$lon = $coordinates[0]['longitude'];
 
-    $get_data = callAPI('GET', 'https://api.open-meteo.com/v1/forecast?latitude='. $lat .'&longitude='. $lon .'&current=temperature_2m', false, '');
-
-    $response = json_decode($get_data, true);
-
-    $weatherXml = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
-
-    array_to_xml($response,$weatherXml);
-
-
-
-    // Extract relevant information
-    $latitude = $weatherXml->latitude;
-    $longitude = $weatherXml->longitude;
-    $temperature = $weatherXml->current->temperature_2m;
-
-    // Create a new SimpleXMLElement for the response
-    $xmlResponse = new SimpleXMLElement('<weather></weather>');
-
-    // Add weather data to the XML document
-    $xmlResponse->addChild('location', $weatherXml->latitude . ', ' . $weatherXml->longitude);
-    $xmlResponse->addChild('temperature', $weatherXml->current->temperature_2m);
+    //$get_data = callAPI('GET', 'https://api.open-meteo.com/v1/forecast?latitude='. $lat .'&longitude='. $lon .'&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset', false, '');
+    function test_weather()
+    {
+        // TESTING -> Berlin
+        $location = 'test_berlin';
+        $lat = '52.52';
+        $lon = '13.419998';
+        $get_data = callAPI('GET', 'https://api.open-meteo.com/v1/forecast?latitude=' . $lat . '&longitude=' . $lon . '&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset', false, '');
 
 
-    // Set the content type to XML
-    header('Content-Type: application/xml');
+        $response = json_decode($get_data, true);
+
+        $weatherXml = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
+
+        array_to_xml($response, $weatherXml);
+
+        print_r($weatherXml);
 
 
-    $result = $xmlResponse->asXML('./weather_data/'. $location .'.xml');
+        // Extract weather information
+        $latitude = $weatherXml->latitude;
+        $longitude = $weatherXml->longitude;
+        $temperature_max = $weatherXml->temperature_2m_max;
+        $temperature_min = $weatherXml->temperature_2m_min;
+        $sunrise = $weatherXml->sunrise;
+        $sunset = $weatherXml->sunset;
+        // extract weather code
+        $weather_code = $weatherXml->weather_code;
 
-    // Output the XML document
-    echo $xmlResponse->asXML();
-} else {
-    // Location not provided in the GET request
-    echo "Please provide a location.";
+        print_r($weather_code);
+
+        // Create a new SimpleXMLElement for the response
+        $xmlResponse = new SimpleXMLElement('<weather></weather>');
+
+        // Add weather data to the XML document
+        $xmlResponse->addChild('location', $weatherXml->latitude . ', ' . $weatherXml->longitude);
+        $xmlResponse->addChild('temperature', $weatherXml->current->temperature_2m);
+
+
+        // Set the content type to XML
+        header('Content-Type: application/xml');
+
+
+        $result = $xmlResponse->asXML('./weather_data/' . $location . '.xml');
+
+        // Output the XML document
+        echo $xmlResponse->asXML();
+    }
 }
+
+else {
+        // Location not provided in the GET request
+        echo "Please provide a location.";
+    }
 
 
 
